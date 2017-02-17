21520a8739cc7f3b0d3be3cf96133cf3b9460893
Silvio Rahle <silvio.rahle@acceleratio.hr>
2017-02-17 13:08:41 +0100

    PowerShell scripts

---
+++ _dist/AD/DC_ComputersByOUs.ps1
@@ -0,0 +1,50 @@
+﻿###############
+# SYSKIT
+# www.syskit.com
+<#
+Displays a list of all computers in a domain and their corresponding Organizational Units. Run on Domain Controller.
+#>
+################
+function GetComputerObject($comp, $orgUnit)
+{
+    $obj = new-object psObject
+    $obj| Add-Member @Member -Name "Organizational Unit" -Value $orgUnit
+    if ($comp.DNSHostName)
+    {
+        $obj| Add-Member @Member -Name "Computer" -Value $comp.DNSHostName
+    }
+    else
+    {
+        $obj| Add-Member @Member -Name "Computer" -Value $comp.Name
+    }
+	return $obj
+}
+
+$allData= @()
+$data= @()
+$Member = @{
+    MemberType = "NoteProperty"
+    Force = $true
+}
+
+$defaultPartition = Get-ADDomainController | select DefaultPartition
+$computersContainer = "CN=Computers," + $defaultPartition.DefaultPartition
+$computers = Get-ADComputer -Filter * -SearchBase $computersContainer
+foreach ($computer in $computers)
+{
+    $data = GetComputerObject -comp $computer -orgUnit "Computers"
+    $allData += $data
+}
+
+$ous = Get-ADOrganizationalUnit -Filter *
+foreach ($ou in $ous)
+{
+    $computers = Get-ADComputer -Filter * -SearchBase $ou
+    foreach ($computer in $computers)
+    {
+        $data = GetComputerObject -comp $computer -orgUnit $ou.Name
+        $allData += $data
+    }
+}
+
+$allData
